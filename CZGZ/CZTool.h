6232a5ecd315101ea493381d6fb9911c609c2c42
ns <User@192.168.199.128>
2012-07-08 17:29:52 +0800

    定义了工具类

---
+++ CZGZ/CZTool.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface CZTool : NSObject
+
+// 计算2个经纬度之间的距离，计算结果的单位时公里
++ (double)distanceFrom: (CLLocationCoordinate2D)coordinate1 to: (CLLocationCoordinate2D)coordinate2;
+
+@end
---
+++ CZGZ/CZTool.m
@@ -0,0 +1,18 @@
+//
+//  CZTool.m
+//  CZGZ
+//
+//  Created by Lion User on 08/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CZTool.h"
+
+@implementation CZTool
+
++ (double)distanceFrom: (CLLocationCoordinate2D)coordinate1 to: (CLLocationCoordinate2D)coordinate2
+{
+    return 0;
+}
+
+@end

434e3ca9d5b549e266d24015c50dec603153bb99
ns <User@Lions-Mac.local>
2012-07-09 09:12:55 +0800

    ...

--- CZGZ/CZTool.h
+++ CZGZ/CZTool.h
@@ -3,7 +3,7 @@
 
 @interface CZTool : NSObject
 
-// 计算2个经纬度之间的距离，计算结果的单位时公里
+// 计算2个经纬度之间的距离，计算结果的单位时公里  leitianbo
 + (double)distanceFrom: (CLLocationCoordinate2D)coordinate1 to: (CLLocationCoordinate2D)coordinate2;
 
 @end
